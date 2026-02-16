148f6b9a95db6212eb67e1e0ce91c46fdd06478b
idea64 <xsolluch@gmail.com>
2026-02-16 16:29:43 +0500

    Update gui.h

--- src/gui.h
+++ src/gui.h
@@ -20,32 +20,4 @@ namespace gui
 
 	extern gui_state_t state;
 }
-
-//#include <cstdio>
-//
-//#include <Windows.h>
-//#include "imgui/imgui.h"
-//#include "imgui/imgui_impl_win32.h"
-//#include "imgui/imgui_impl_opengl3.h"
-//
-//struct values_t
-//{
-//	bool esp_enabled;
-//};
-//
-//namespace gui
-//{
-//	extern values_t *vals;
-//
-//	bool init(HWND wnd);
-//	void shutdown();
-//
-//	void draw();
-//
-//	bool get_is_init();
-//	bool get_do_draw();
-//
-//	void set_do_draw(bool draw);
-//}
-
 #endif
